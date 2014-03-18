ffcd6b1aaed88516cf40c899df8700ce4c53bfa3
Ralph Miller <-->
2014-03-17 23:04:24 -0700

    PFAB Homework 1
    First homework assignment for Programing for Absolute Beginners with
    Mark Lassoff

---
+++ Homework.py
@@ -0,0 +1,22 @@
+#This is Ralph Miller's Homework for Programming for Absolute Beginners with Mark Lassoff, completed March 17, 2014
+#Homework problem 10
+first = "Ralph"
+last = "Miller"
+age = 37
+print first , last , age
+#Homework problem 13
+sideA = 12.55
+sideB = 17.85
+Csquared = (sideA * sideA) + (sideB * sideB)
+import math
+value = math.sqrt(Csquared);
+print value;
+#Homework problem 15
+operand1 = 95
+operand2 = 64.5
+print operand1 , "+" , operand2 , "=" , operand1 + operand2
+print operand1 , "-" , operand2 , "=" , operand1 - operand2
+print operand1 , "*" , operand2 , "=" , operand1 * operand2
+print operand1 , "/" , operand2 , "=" , operand1 / operand2
+print operand1 , "%" , operand2 , "=" , operand1 % operand2
+#this soultion is best because if either operand changes, no code needs to be rewritten
