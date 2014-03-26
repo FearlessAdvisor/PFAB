9b0f3f3048c32d7dff329bf9a5511b2cb5296119
Ralph Miller <-->
2014-03-26 02:07:14 -0700

    Added comments

--- Homework2A.py
+++ Homework2A.py
@@ -1,12 +1,15 @@
 #This is Ralph Miller's Homework #2A for Programming for Absolute Beginners with Mark Lassoff, completed March 25, 2014
 
+#I know this isn't exactly what you were looking for, but I wanted to to figure out how to prompt users for test scores and save them to a list.
 testScores = list() 
 n = int(input('How many test scores do you want to enter? ')) 
 for i in range(n): 
     testScore = input('Enter your test score: ') 
     testScores.append(testScore) 
+#let's find the average of all test scores
 average = sum(testScores)/n
 print 'The average of your test scores is:' , average
+#now let's determine the letter grade
 if average >= 90:
     grade = 'A'
 elif average >= 80:
--- Homework2B.py
+++ Homework2B.py
@@ -1,9 +1,10 @@
 #This is Ralph Miller's Homework #2B for Programming for Absolute Beginners with Mark Lassoff, completed March 25, 2014
+
+#let's set locale to show US currency and show only 2 decimal places
 import locale
 locale.setlocale( locale.LC_ALL, '' )
 
-
 itemPrice = input('What is the price of the item?: ')
-salesTax = input('What is the sales tax? (expressed as 0.XX)?: ')
+salesTax = input('What is the sales tax rate? (expressed as 0.XX)?: ')
 totalPrice = itemPrice + (itemPrice * salesTax)
 print 'The full price of your item is' , locale.currency(totalPrice)
